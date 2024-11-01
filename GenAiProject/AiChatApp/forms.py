4b16cb92aec4025934c3aada07369949e6729651
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-11-01 15:17:32 -0300

    melhoras no register e login

--- GenAiProject/AiChatApp/forms.py
+++ GenAiProject/AiChatApp/forms.py
@@ -1,17 +1,30 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Usuário', 'class': 'form-control'}),
+        label='Usuário'
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Senha', 'class': 'form-control'}),
+        label='Senha'
+    )
+
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}),
+        label='Email'
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.is_superuser = False
-        user.is_staff = False
-        if commit:
-            user.save()
-        return user
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder': 'Usuário', 'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Senha', 'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirme a senha', 'class': 'form-control'})
